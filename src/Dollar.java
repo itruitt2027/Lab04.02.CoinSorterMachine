fdfa9528873047134d71caa47dc7bdc09db524e9
itruitt2027 <itruitt2027@gmail.com>
2023-04-06 11:42:03 -0400

    Initial commit

---
+++ src/Dollar.java
@@ -0,0 +1,11 @@
+public class Penn extends Coin {
+    @Override
+    public double getValue() {
+        return .01;
+    }
+
+    @Override
+    public String getName() {
+        return "penny";
+    }
+}
